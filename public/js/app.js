4b58c64ac2f652262ac2c4264dd590fd7d975956
UmbertoV <umberto.ottone@gmail.com>
2020-01-18 15:32:39 +0100

    Impostazioni JS cambio icona della chat

--- public/js/app.js
+++ public/js/app.js
@@ -10705,7 +10705,19 @@ return jQuery;
 
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  // intercetto il click sull'icona fissa della Chat
+  $('.open-m').click(function () {
+    // faccio scomparire l'icona del mess e compare la X
+    $('.open-m').addClass('disactive');
+    $('.close-m').addClass('active');
+  });
+  $('.close-m').click(function () {
+    // faccio scomparire l'icona della x e compare quella del mess
+    $('.close-m').removeClass('active');
+    $('.open-m').removeClass('disactive');
+  });
+});
 
 /***/ }),
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 var $ = require('jquery');
 
 $(document).ready(function() {
-    
+    // intercetto il click sull'icona fissa della Chat
+    $('.open-m').click(function(){
+        // faccio scomparire l'icona del mess e compare la X
+        $('.open-m').addClass('disactive');
+        $('.close-m').addClass('active');
+
+    })
+    $('.close-m').click(function(){
+
+        // faccio scomparire l'icona della x e compare quella del mess
+        $('.close-m').removeClass('active');
+        $('.open-m').removeClass('disactive');
+    })
 });
